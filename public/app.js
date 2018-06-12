0cba7671c27e0062545f713fa4e07d0bc2aa781d
Edward Fallon <eddie_fallon@hotmail.com>
2018-06-12 21:29:34 +0100

    initial commit

---
+++ public/app.js
@@ -0,0 +1,19 @@
+const drawMap = function(){
+  const mapDiv = document.getElementById('main-map');
+  const glasgow = [55.86515, -4.25763];
+  const javaStreet = [55.801507, -4.003805];
+  const zoomLevel = 15;
+  const mainMap = new MapWrapper(mapDiv, javaStreet, zoomLevel);
+  // mainMap.addMarker(javaStreet);
+  // var button = document.querySelector('button');
+  // button.addEventListener('click', handleButtonClick);
+};
+
+// var handleButtonClick = function () {
+//   this.mainMap.panToSomewhere(glasgow)
+//
+// }
+
+
+
+window.addEventListener('load', drawMap);
